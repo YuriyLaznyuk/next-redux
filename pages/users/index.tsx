ee0619294e8db153ee949c6709bb5a5b6582068f
YuriyLaznyuk <yuriy.laznyuk@gmail.com>
2022-01-03 18:49:03 +0200

    fix: error

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import styles from './users.module.scss';
 import { useAppDispatch, useAppSelector } from '../../src/hooks/hooks';
-import { fetchUsers, clearUser } from './usersLogic';
+import { error, loading, success, clear } from '../../src/store/reducers/usersSlise';
+import axios from 'axios';
+// import { fetchUsers, clearUser } from './usersLogic';
 
 const Users = () => {
 	const dispatch = useAppDispatch();
-	const { users, error, loading } = useAppSelector(state => state.users);
-
+	const data = useAppSelector(state => state.users);
+	const fetchUsers = async () => {
+		try {
+			dispatch(loading());
+			const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+			dispatch(success(response.data));
+		} catch {
+			dispatch(error());
+		}
+	};
 	return (
 		<div className={styles.users}>
-			<button className={styles.users__btn} onClick={() => fetchUsers(dispatch)}>
+			<button className={styles.users__btn} onClick={() => fetchUsers()}>
 				Show User
 			</button>
-			<button className={styles.users__btn} onClick={() => clearUser(dispatch)}>
+			<button className={styles.users__btn} onClick={() => dispatch(clear())}>
 				Clear User
 			</button>
 			<div className={styles.users__show}>
-				{loading && <h3>...Loading</h3>}
-				{error && <h3>Error loading</h3>}
-				{users.length > 0 &&
-					users.map(i => (
+				{data.loading && <h3>...Loading</h3>}
+				{data.error && <h3>Error loading</h3>}
+				{data.users.length > 0 &&
+					data.users.map(i => (
 						<div className={styles.users__show_item} key={i.id}>
 							{i.name}--{i.email}--{i.address.city}
 						</div>
